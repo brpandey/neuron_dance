d3006f65fe3ad5669bfa6efd8f39f3dd58b2c1a3
Bibek Pandey <bibekp@gmail.com>
2024-01-30 15:31:35 -0800

    Added FromStr impl to create specific Activation trait objects from supplied strings

--- src/activation/functions.rs
+++ src/activation/functions.rs
@@ -1,2 +1,34 @@
+// To add new activation type update 3 places:
+// Add new activation module type here - Start (1)
 pub mod relu;
 pub mod sigmoid;
+// End Finish (1)
+
+use std::str::FromStr;
+use std::fmt;
+use crate::activation::{Activation,
+                        functions::{relu::Relu, sigmoid::Sigmoid}}; // Update (2)
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct ActivationParseError;
+
+// manual error msg implementation rather than pulling in external crate
+impl fmt::Display for ActivationParseError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Unknown activation type")
+    }
+}
+
+impl FromStr for Box<dyn Activation> {
+    type Err = ActivationParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        // Add new activation type here - Start (3)
+        match s {
+            "relu" => Ok(Box::new(Relu)),
+            "sigmoid" => Ok(Box::new(Sigmoid)),
+            _ => Err(ActivationParseError),
+        }
+        // End Finish (3)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use simple_network::{
     network::Network,
-    activation::functions::{relu::Relu, sigmoid::Sigmoid},
+    activation::Activation,
     dataset::DataSet
 };
 
@@ -16,6 +16,9 @@ fn main() {
 //    println!("train X  {:?} -------- train Y {:?}", &x_train, &y_train);
 //    println!("test X {:?} --------- test Y {:?}", &x_test, &y_test);
 
-    let mut simple_nn = Network::new(vec![3, 3, 1], vec![Box::new(Relu), Box::new(Sigmoid)], 0.01);
+    // construct activation function trait objects from supplied strs
+    let layers = vec!["relu", "sigmoid"].into_iter().map(|l| l.parse().unwrap()).collect::<Vec<Box<dyn Activation>>>();
+
+    let mut simple_nn = Network::new(vec![3, 3, 1], layers, 0.01);
     simple_nn.train_sgd(&x_train, &y_train, n_train, &x_test, &y_test, n_test);
 }
