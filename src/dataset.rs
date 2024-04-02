e748c240a2b9bbc4c634336def36db3bdae07e1a
Bibek Pandey <bibekp@gmail.com>
2024-04-01 20:39:07 -0700

    Added simple feature scaling routine to dataset using min max scale

--- src/dataset.rs
+++ src/dataset.rs
@@ -1,4 +1,4 @@
-use ndarray::Array2;
+use ndarray::{Array2, Axis};
 use std::{env, fmt};
 
 pub mod csv;
@@ -26,6 +26,29 @@ pub struct SubsetRef<'a> {
 pub type TrainTestSubsetRef<'a> = (SubsetRef<'a>, SubsetRef<'a>);
 
 impl TrainTestSubsetData {
+
+    // scale is 0 min 1 max
+    pub fn min_max_scale(&self, min: f64, max: f64) -> Self {
+        let x_train = &self.0.0;
+        let x_test = &self.0.3;
+
+        // get the min values for each column
+        let x_train_mins = x_train.map_axis(Axis(0), |v| *v.iter().min_by(|a,b| a.total_cmp(b)).unwrap());
+        let x_test_mins = x_test.map_axis(Axis(0), |v| *v.iter().min_by(|a,b| a.total_cmp(b)).unwrap());
+
+        let x_train_maxs = x_train.map_axis(Axis(0), |v| *v.iter().max_by(|a,b| a.total_cmp(b)).unwrap());
+        let x_test_maxs = x_test.map_axis(Axis(0), |v| *v.iter().max_by(|a,b| a.total_cmp(b)).unwrap());
+
+        let x_train_std = (x_train - &x_train_mins) / (&x_train_maxs - &x_train_mins);
+        let x_test_std = (x_test - &x_test_mins) / (&x_test_maxs - &x_test_mins);
+
+        let x_train_scaled = x_train_std*(max-min) + min;
+        let x_test_scaled = x_test_std*(max-min) + min;
+
+        TrainTestSubsetData((x_train_scaled, self.0.1.clone(), self.0.2, x_test_scaled, self.0.4.clone(), self.0.5))
+    }
+
+
     pub fn get_ref<'a>(&'a self) -> TrainTestSubsetRef<'a> {
         (
             SubsetRef {
